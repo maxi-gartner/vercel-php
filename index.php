e3ea51619f67d2fcb08260745ae68bbe3d1d8def
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-27 09:48:47 -0300

    sad

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
-/* include('./controladores/configuracion.php'); */
+require_once(__DIR__ . '/vendor/autoload.php');
 
-/* $sql_usuarios = "SELECT us.id_usuario, us.nombres, us.apellido, us.email, rol.rol FROM tb_usuarios as us inner join tb_roles as rol on us.id_rol = rol.id_rol";
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('SERVIDOR', $_ENV['SERVIDOR']);
+define('USUARIO', $_ENV['USUARIO']);
+define('PASSWORD', $_ENV['PASSWORD']);
+define('BD', $_ENV['BD']);
+
+$servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
+
+try {
+    $pdo = new PDO(
+        $servidor, 
+        USUARIO, 
+        PASSWORD, 
+        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
+    );
+} catch (PDOException $e) {
+    echo "Error al conectar a la base de datos: " . $e->getMessage();
+}
+
+$sql_usuarios = "SELECT us.id_usuario, us.nombres, us.apellido, us.email, rol.rol FROM tb_usuarios as us inner join tb_roles as rol on us.id_rol = rol.id_rol";
 $query_usuarios = $pdo->prepare($sql_usuarios);
 $query_usuarios->execute();
-$datos_usuarios = $query_usuarios->fetchAll(PDO::FETCH_ASSOC); */
+$datos_usuarios = $query_usuarios->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
