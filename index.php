bfb4766832f91ef22404bd5c4a08fd3ed6d1a64a
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-27 10:11:59 -0300

    asdasd

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
-require_once(__DIR__ . '/vendor/autoload.php');
-
-use Dotenv\Dotenv;
-
-$dotenv = Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-define('SERVIDOR', $_ENV['SERVIDOR']);
-define('USUARIO', $_ENV['USUARIO']);
-define('PASSWORD', $_ENV['PASSWORD']);
-define('BD', $_ENV['BD']);
-
-$servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
-
-try {
-    $pdo = new PDO(
-        $servidor, 
-        USUARIO, 
-        PASSWORD, 
-        array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-    );
-} catch (PDOException $e) {
-    echo "Error al conectar a la base de datos: " . $e->getMessage();
-}
+require_once(__DIR__ . '/controladores/configuracion.php');
 
 $sql_usuarios = "SELECT us.id_usuario, us.nombres, us.apellido, us.email, rol.rol FROM tb_usuarios as us inner join tb_roles as rol on us.id_rol = rol.id_rol";
 $query_usuarios = $pdo->prepare($sql_usuarios);
