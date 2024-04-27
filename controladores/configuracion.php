c72eacb74b904ff81dac3b3edd51ad1c3f82541d
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-27 09:41:43 -0300

    ya ni se

--- controladores/configuracion.php
+++ controladores/configuracion.php
@@ -1,17 +1,15 @@
 <?php
-$RUTA = __DIR__ . "/../"; // Obtener la ruta absoluta del directorio padre
-require_once($RUTA . 'vendor/autoload.php'); // Incluir el archivo usando la ruta absoluta
+require_once(__DIR__ . '/vendor/autoload.php');
 
 use Dotenv\Dotenv;
 
-$dotenv = Dotenv::createImmutable($RUTA); // Usar la misma ruta para cargar el archivo .env
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 define('SERVIDOR', $_ENV['SERVIDOR']);
 define('USUARIO', $_ENV['USUARIO']);
 define('PASSWORD', $_ENV['PASSWORD']);
 define('BD', $_ENV['BD']);
-
 
 $servidor = "mysql:dbname=" . BD . ";host=" . SERVIDOR;
 
@@ -28,4 +26,8 @@ try {
 
 date_default_timezone_set('America/Argentina/Buenos_Aires');
 $fechaHora = date('Y-m-d H:i:s');
+
+$URL = "http://localhost/VentasPHPyMySql/";
+
+
 ?>
