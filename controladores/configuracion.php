c04498191524b61124fbf67531b50fe98fbe2f46
Maximiliano Gartner <113448691+maxi-gartner@users.noreply.github.com>
2024-04-27 10:59:46 -0300

    asfsaf

--- controladores/configuracion.php
+++ controladores/configuracion.php
@@ -2,46 +2,8 @@
 
 namespace controladores;
 
-use Dotenv\Dotenv;
-use PDO;
-use PDOException;
-
 class configuracion {
-    private static $pdo;
-
-    public static function inicializar() {
-        $dotenv = Dotenv::createImmutable(__DIR__);
-        $dotenv->load();
-
-        $servidor = "mysql:dbname=" . $_ENV['BD'] . ";host=" . $_ENV['SERVIDOR'];
-
-        try {
-            self::$pdo = new PDO(
-                $servidor, 
-                $_ENV['USUARIO'], 
-                $_ENV['PASSWORD'], 
-                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
-            );
-        } catch (PDOException $e) {
-            echo "Error al conectar a la base de datos: " . $e->getMessage();
-            // Aquí quizás deberías lanzar una excepción en lugar de simplemente mostrar el mensaje
-        }
-
-        date_default_timezone_set('America/Argentina/Buenos_Aires');
-    }
-
-    public static function getPdo() {
-        if (!isset(self::$pdo)) {
-            self::inicializar();
-        }
-        return self::$pdo;
-    }
-
-    public static function getFechaHora() {
-        return date('Y-m-d H:i:s');
-    }
-
-    public static function getURL() {
-        return "http://localhost/VentasPHPyMySql/";
+    public function saludar() {
+        echo 'Hola mundo';
     }
 }
--- index.php
+++ index.php
@@ -8,55 +8,7 @@ spl_autoload_register(function ($clase) {
     }
 });
 
-$sql_usuarios = "SELECT us.id_usuario, us.nombres, us.apellido, us.email, rol.rol FROM tb_usuarios as us inner join tb_roles as rol on us.id_rol = rol.id_rol";
-$query_usuarios = $pdo->prepare($sql_usuarios);
-$query_usuarios->execute();
-$datos_usuarios = $query_usuarios->fetchAll(PDO::FETCH_ASSOC);
 
+$configuracion = new configuracion;
+$configuracion->saludar();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Usuarios</title>
-</head>
-<body>
-    <h1>Lista de Usuarios</h1>
-    <a href="<?= $URL ?>paginas/pagina1.php">Pagina 1</a>
-    <a href="<?= $URL ?>paginas/pagina2.php">Pagina 2</a>
-    <a href="<?= $URL ?>paginas/pagina3.php">Pagina 3</a>
-    <table id="example1" class="table table-bordered table-striped">
-        <thead>
-            <tr>
-                <th>Nro</th>
-                <th>Nombre</th>
-                <th>Apellido</th>
-                <th>Email</th>
-                <th>Rol</th>
-                <th>Acciones</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($datos_usuarios as $dato_usuario): ?>
-            <tr>
-                <td><?= ++$contador ?></td>
-                <td><?= ucfirst($dato_usuario['nombres']) ?></td>
-                <td><?= ucfirst($dato_usuario['apellido']) ?></td>
-                <td><?= $dato_usuario['email'] ?></td>
-                <td><?= ucfirst($dato_usuario['rol']) ?></td>
-                <td>
-                    <a href="<?= $URL ?>paginas_usuarios/details_user.php?id=<?= $dato_usuario['id_usuario'] ?>">Ver</a>
-                    <a href="<?= $URL ?>paginas_usuarios/update_user.php?id=<?= $dato_usuario['id_usuario'] ?>">Editar</a>
-                    <form action="<?= $URL ?>app/controllers/usuarios/delete_user_controller.php" method="post" style="display: inline;">
-                        <input type="hidden" name="id_usuario" value="<?= $dato_usuario['id_usuario'] ?>">
-                        <input type="hidden" name="nombre_usuario" value="<?= $dato_usuario['nombres'] ?>">
-                        <button type="submit">Borrar</button>
-                    </form>
-                </td>
-            </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</body>
-</html>
